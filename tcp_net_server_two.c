e4d0ba7a689f490ba0b417df1c98e1fd9e108af6
taishi <791974929@qq.com>
2017-05-03 21:33:10 +0800

    two

--- tcp_net_server_two.c
+++ tcp_net_server_two.c
@@ -1,19 +1,27 @@
 #include "tcp_net_socket.h"
-void init_daemon(void);//守护进程初始化函数
+
 int main(int argc, char* argv[])
 {
-	if(argc < 3)
-	{
-		printf ("usage:./servertcp ip port\n");
-		exit(-1);
-	}
 	// init_daemon();
-	int sdf = tcp_init(argv[1], atoi(argv[2]));
+	config_file_testing(argv[0]);
+	char ip[15],port[5];
+	FILE *xp=fopen("server.ini","r");
+	if (xp != NULL){
+		fseek(xp,0L,SEEK_SET);
+		fscanf(xp,"%s %s",ip,port);
+		fclose(xp);
+	}else {
+		printf ("open file error");
+		exit(0);
+	}
+	int sdf = tcp_init(ip, atoi(port));
 	while(1)
 	{
 		int cfd = tcp_accept(sdf);
+		int ddd[100],i=0;
+		ddd[i] = cfd;
 		char buf[100] = {0};
-		char buf_one[] = {0};
+		char buf_one[100] = {0};
 		if (recv(cfd, buf, sizeof(buf), 0) == -1)//从客户端接收数据存于buf中
 		{
 			perror("recv");
@@ -22,32 +30,24 @@ int main(int argc, char* argv[])
 			exit(-1);
 		}
 		puts(buf);
-		FILE *xp=fopen("server.ini","a+");
-		// fprintf(xp,"%s",buf);
-		fseek(xp,0L,SEEK_SET);
-		fscanf(xp,"%[^EOF]",&buf_one);
-		puts(buf_one);
-		fclose(xp);
-		if (1){
-			if (send(cfd, buf_one, sizeof(buf_one), 0) == -1)//从buf中取向cfd客户端发送数据
-			{
-				perror("send");
-				close(cfd);
-				close(sdf);
-				exit(-1);
-			}
+		if (send(cfd, buf, sizeof(buf), 0) == -1)//从buf中取向cfd客户端发送数据
+		{
+			perror("send");
+			close(cfd);
+			close(sdf);
+			exit(-1);
 		}
-		else{
-			char no[]={"Parameter does not exist"};
-			if (send(cfd, no, sizeof(no), 0) == -1)//从buf中取向cfd客户端发送数据
-			{
-				perror("send");
-				close(cfd);
-				close(sdf);
-				exit(-1);
-			}
+		// char no[]={"Parameter does not exist"};
+		if (send(cfd, "Parameter does not exist", sizeof("Parameter does not exist"), 0) == -1)//从buf中取向cfd客户端发送数据
+		{
+			perror("send");
+			close(cfd);
+			close(sdf);
+			exit(-1);
 		}
 		close(cfd);
+		printf ("%d\n",ddd[i]);
+		i++;
 	}
 	close(sdf);
 	return 0;
