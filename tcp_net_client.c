cdf9419b904aa6da42bc322a85427f65c3666a9a
taishi <791974929@qq.com>
2017-05-04 23:35:52 +0800

    three

--- tcp_net_client.c
+++ tcp_net_client.c
@@ -1,11 +1,9 @@
 #include "tcp_net_socket.h"
-//线程函数
-void thread_1(void)  
-{  
-	time_t timep;  
-    char s[30];  
-    time(&timep);  
-    strcpy(s,ctime(&timep));
+
+//主函数
+int main (int argc, char *argv[])
+{
+	config_file_testing(argv[0]);
 	char ip[15],port[5];
 	FILE *xp=fopen("cliten.ini","r");
 	if (xp != NULL){
@@ -18,25 +16,9 @@ void thread_1(void)
 	}
 	char buf[100]={0};
     int sdf = tcp_connect(ip,atoi(port));
-	send(sdf, s, strlen(s), 0);
+	send(sdf, "test", strlen("test"), 0);
 	recv(sdf, buf, sizeof(buf), 0);
-	printf("%s\n",buf);
+	puts(buf);
 	close(sdf);
-} 
-//主函数
-int main (int argc, char *argv[])
-{
-	config_file_testing(argv[0]);
-	pthread_t id;
-	int i,ret;
-	for (i=0;i<2;i++){
-		ret = pthread_create(&id,NULL,(void  *) thread_1,NULL);
-		if (ret != 0){
-			printf("Create pthread error!\n");  
-			return -1;
-		}
-		pthread_join(id,NULL);
-		sleep(1);
-	}
-return 0;
+	return 0;
 }
