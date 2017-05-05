3f72d5a09f4431f6a1f96b3064a341e1cd24c1b4
taishi <791974929@qq.com>
2017-05-05 19:55:48 +0800

    three

--- mysql/mysql.c
+++ mysql/mysql.c
@@ -16,34 +16,34 @@ int main (int argc, char *argv[])
   
 		conn_ptr = mysql_real_connect(conn_ptr, "localhost", "yangguo","accp132accp","oktest", 0, NULL, 0); //建立实际连接  
 		//参数分别为：初始化的连接句柄指针，主机名（或者IP），用户名，密码，数据库名，0，NULL，0）后面三个参数在默认安装mysql>的情况下不用改  
-		if(conn_ptr){  
-				printf("Connection success\n");  
-				printf("%d\n",conn_ptr);  
-				}  
-		else {  
-				printf("Connection failed\n");  
-		}  
+		if(conn_ptr){
+				printf("Connection success\n");
+				printf("%d\n",conn_ptr);
+				}
+		else {
+				printf("Connection failed\n");
+		}
 	
 		query = "select * from student";
-		/*查询，成功则返回0*/  
-		flag = mysql_real_query(&mysql, query, (unsigned int)strlen(query));  
-		if(flag) {  
-			printf("Query failed!\n");  
-			return 0;  
+		/*查询，成功则返回0*/
+		flag = mysql_real_query(&mysql, query, (unsigned int)strlen(query));
+		if(flag) {
+			printf("Query failed!\n");
+			return 0;
 		}else{
-			printf("[%s] made...\n", query);  
-		}  
-		/*mysql_store_result讲全部的查询结果读取到客户端*/  
-		res = mysql_store_result(conn_ptr);  
-		/*mysql_fetch_row检索结果集的下一行*/  
-		while(row = mysql_fetch_row(res)) {  
-		/*mysql_num_fields返回结果集中的字段数目*/  
-			for(t=0; t<mysql_num_fields(res); t++)  
-			{  
-				printf("%s\t", row[t]);  
-			}  
+			printf("[%s] made...\n", query);
+		}
+		/*mysql_store_result讲全部的查询结果读取到客户端*/
+		res = mysql_store_result(conn_ptr);
+		/*mysql_fetch_row检索结果集的下一行*/
+		while(row = mysql_fetch_row(res)) {
+		/*mysql_num_fields返回结果集中的字段数目*/
+			for(t=0; t<mysql_num_fields(res); t++)
+			{
+				printf("%s\t", row[t]);
+			}
 			printf("\n");
 		}
-		mysql_close(conn_ptr); //关闭连接  
-		return EXIT_SUCCESS;  
+		mysql_close(conn_ptr);//关闭连接
+		return EXIT_SUCCESS;
 }  
